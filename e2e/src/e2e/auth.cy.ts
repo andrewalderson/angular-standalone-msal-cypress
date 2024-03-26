506f95b93753723172c443815b40bb99218dcb66
Andrew Alderson <andrew.alderson@outlook.com>
2024-03-26 22:51:17 +0000

    chore: add msal auth pages

---
+++ e2e/src/e2e/auth.cy.ts
@@ -0,0 +1,38 @@
+/**
+ * These tests are sanity checks to ensure the routes needed for MSAL to function
+ * have been defined in the app.
+ */
+describe('Auth', () => {
+  beforeEach(() => {
+    // We are logging in so that we don't get redirected to the AD B2C login page
+    // when we attempt to go back to the home route. This will happen if there is an MSALGuard on the home route
+    // we also need to visit the home page to trigger the fetch of the msl configuration
+    cy.login().visit('/');
+  });
+
+  it('should be able to route to the sign in redirect page', () => {
+    cy.wait('@getMsalConfig')
+      .its('response.body.auth.redirectUri')
+      .then((route) => {
+        cy.visit(route);
+        cy.get('app-redirect').should('exist');
+      });
+  });
+  it('should be able to route to the login failed page', () => {
+    cy.wait('@getMsalConfig')
+      .its('response.body.guard.loginFailedRoute')
+      .then((route) => {
+        cy.visit(route);
+        cy.get('app-login-failed').should('exist');
+      });
+  });
+
+  it('should be able to route to the signed out page', () => {
+    cy.wait('@getMsalConfig')
+      .its('response.body.auth.postLogoutRedirectUri')
+      .then((route) => {
+        cy.visit(route);
+        cy.get('app-signed-out').should('exist');
+      });
+  });
+});
--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -4,4 +4,14 @@ import { MsalRedirectComponent } from '@azure/msal-angular';
 // Routes specific to the Msal library
 export const authRoutes: Route[] = [
   { path: '_signin-callback', component: MsalRedirectComponent },
+  {
+    path: '_login-failed',
+    loadComponent: async () =>
+      (await import('./pages/login-failed/login-failed.page')).LoginFailedPage,
+  },
+  {
+    path: '_signed-out',
+    loadComponent: async () =>
+      (await import('./pages/signed-out/signed-out.page')).SignedOutPage,
+  },
 ];
