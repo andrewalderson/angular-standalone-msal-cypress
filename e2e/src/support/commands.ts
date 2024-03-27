bee42760e005222e1470c525ac229aedaec390f9
Andrew Alderson <andrew.alderson@outlook.com>
2024-03-27 20:36:00 +0000

    refactor: add options to msal login command

--- e2e/src/support/commands.ts
+++ e2e/src/support/commands.ts
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { MsalLoginOptions } from './commands/msal-login';
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -9,18 +11,19 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-
-// eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Chainable<Subject> {
-    login(): Chainable;
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable {
+      login(options?: Partial<MsalLoginOptions>): Chainable<Element>;
+    }
   }
 }
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', () => {
-  return cy.msalLogin(Cypress.env('msal'));
+Cypress.Commands.add('login', (options: Partial<MsalLoginOptions> = {}) => {
+  return cy.msalLogin({ ...Cypress.env('msal'), ...options });
 });
 //
 // -- This is a child command --
