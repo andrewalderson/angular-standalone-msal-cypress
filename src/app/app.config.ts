ee35914147ec2d3dff306214b4ce02b5eb5d33b5
Andrew Alderson <andrew.alderson@outlook.com>
2024-04-05 20:55:40 +0000

    refactor: move msal tokens to seperate file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,93 +12,14 @@ import {
   inject,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import {
-  MsalGuardConfiguration,
-  MsalInterceptorConfiguration,
-} from '@azure/msal-angular';
-import { Configuration, LogLevel } from '@azure/msal-browser';
 import { appRoutes } from './app.routes';
+import {
+  _MSAL_CONFIG,
+  _MSAL_GUARD_CONFIG,
+  _MSAL_INSTANCE_CONFIG,
+  _MSAL_INTERCEPTOR_CONFIG,
+} from './auth/msal.tokens';
 import { provideMsal, withGuard, withInterceptor } from './auth/provide-msal';
-
-export function loggerCallback(
-  logLevel: LogLevel,
-  message: string,
-  containsPii: boolean
-) {
-  if (containsPii) {
-    return;
-  }
-  switch (logLevel) {
-    case LogLevel.Error:
-      console.error(message);
-      break;
-    case LogLevel.Info:
-      // eslint-disable-next-line no-restricted-syntax
-      console.info(message);
-      break;
-    case LogLevel.Verbose:
-      // eslint-disable-next-line no-restricted-syntax
-      console.debug(message);
-      break;
-    case LogLevel.Warning:
-      console.warn(message);
-      break;
-    case LogLevel.Trace:
-      console.trace(message);
-      break;
-  }
-}
-
-type MsalConfig = Configuration & {
-  guard: MsalGuardConfiguration;
-} & {
-  interceptor: MsalInterceptorConfiguration;
-};
-
-// IMPORTANT careful what you name these tokens so they don't conflict with the tokens defined by Msal
-const _MSAL_CONFIG = new InjectionToken<MsalConfig>('MSAL_CONFIG');
-const _MSAL_INSTANCE_CONFIG = new InjectionToken<Configuration>(
-  '_MSAL_INSTANCE_CONFIG',
-  {
-    providedIn: 'root',
-    factory: () => {
-      const configuration = inject(_MSAL_CONFIG);
-      return {
-        auth: configuration.auth,
-        cache: configuration.cache,
-        system: {
-          ...configuration.system,
-          loggerOptions: {
-            ...configuration.system?.loggerOptions,
-            loggerCallback: loggerCallback,
-          },
-        },
-      };
-    },
-  }
-);
-// MSAL already has a token called 'MSAL_GUARD_CONFIG' so don't use that
-const _MSAL_GUARD_CONFIG = new InjectionToken<MsalGuardConfiguration>(
-  '_MSAL_GUARD_CONFIG',
-  {
-    providedIn: 'root',
-    factory: () => inject(_MSAL_CONFIG).guard,
-  }
-);
-
-const _MSAL_INTERCEPTOR_CONFIG = new InjectionToken<MsalGuardConfiguration>(
-  '_MSAL_INTERCEPTOR_CONFIG',
-  {
-    providedIn: 'root',
-    factory: () => {
-      const configuration = inject(_MSAL_CONFIG).interceptor;
-      return {
-        interactionType: configuration.interactionType,
-        protectedResourceMap: new Map(configuration.protectedResourceMap),
-      };
-    },
-  }
-);
 
 /**
  * Providers cannot be asynchronous so we can't wait for the fetch of
