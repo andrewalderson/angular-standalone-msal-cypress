3302779d69ab86ffce2d572bc95b28723181cfa0
Andrew Alderson <andrew.alderson@outlook.com>
2024-04-07 18:55:17 +0000

    refactor: use InjectionToken for guards
    This makes the code more readable IMHO

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Route } from '@angular/router';
-import { MsalGuard } from '@azure/msal-angular';
 import { authRoutes } from './auth/auth.routes';
+import { MUST_BE_AUTHENTICATED_TO_ACTIVATE } from './auth/must-be-authenticated.guard';
 
 export const appRoutes: Route[] = [
   ...authRoutes,
   {
     path: '',
-    canActivate: [MsalGuard],
+    canActivate: [MUST_BE_AUTHENTICATED_TO_ACTIVATE],
     loadComponent: async () =>
       (await import('./nx-welcome.component')).NxWelcomeComponent,
   },
