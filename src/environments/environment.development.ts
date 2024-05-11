36589756c3b99e44f62b54de96711aa3d6ee7fca
Andrew Alderson <andrew.alderson@outlook.com>
2024-05-11 18:30:06 +0000

    chore: use only one environment file
    In a real world app you shouldn't build seperate artifacts
    for each environment/ You should build one artifact, test it
    and then deploy the same artifact to all environments.
    This is why the msal config is externalized in this application.
    It can be updated and validated seperately from the app.

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  // in real world scenarios this config would probably be loaded from a different api for each environment
-  msalConfigUrl: './assets/msal.config.json',
-};
