1c52174d441f167dbca5674ce239a0dd5161e495
Andrew Alderson <andrew.alderson@outlook.com>
2024-05-11 18:30:58 +0000

    docs: better explain bootstrapping msal

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,23 @@ import { environment } from './environments/environment';
  * here because the router has 'withEnabledBlockingInitialNavigation' set.
  * This prevents APP_INITIALIZER's from resolving until after the initial route
  * navigation. Since we have an MsalGuard on the root route we need the msal config
- * before the app is initialized. These means we can't use a APP_INITIALIZER to fetch it.
- * We will use app.config.ts for configuring the app and simple
+ * before the app is initialized. This means we can't use a APP_INITIALIZER to fetch it.
+ * We will use app.config.ts for configuring the app and simply
  * fetch the config here before bootstrapping and create an InjectionToken with the config
  * The bonus in this is that the redirect for the authentication will happen
  * before the app is bootstrapped
+ *
+ * NOTE - we are externalizing the msal config and fetching it
+ * because we need to intercept it in the Cypress tests to change some
+ * of the values for the login in the tests to work and because
+ * config like this should be external to the application.
+ *
+ * We could imporve the performance of fetching the msal config by either using SSR
+ * and embedding it in the index.html page or adding a Link header to
+ * prefetch it. We could also do this in the CI/CD pipeline because the config is
+ * the same for every user in each environment. We could also add the headers in a CDN.
+ * So many options, but this is something for another day.
+ *
  */
 bootstrapMsalProviders(environment.msalConfigUrl).then((msalProviders) =>
   bootstrapApplication(AppComponent, {
