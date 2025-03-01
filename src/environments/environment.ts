320bb383e5bc4b2cdc514ba1836e6c64067a797f
Andrew Alderson <andrew.alderson@outlook.com>
2025-02-28 20:01:59 -0700

    refactor: use public folder for assets
    New project structure in Angular 19

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   // in real world scenarios this url may or may not be different for each environment.
   // We could have a different endpoint for each environment or we could use the same url
   // and update the msal.config.json file in a CI/CD pipeline when deploying to different environments
-  msalConfigUrl: './assets/msal.config.json',
+  msalConfigUrl: 'msal.config.json',
 };
